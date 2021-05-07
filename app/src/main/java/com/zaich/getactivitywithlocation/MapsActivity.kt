c2f24da298e6089eb8216e81328c3041fca83d49
reza <ichsanireza@gmail.com>
2021-05-07 14:42:42 +0700

    Aplication get activity with current location and time - add Splash screen

--- app/src/main/java/com/zaich/getactivitywithlocation/MapsActivity.kt
+++ app/src/main/java/com/zaich/getactivitywithlocation/MapsActivity.kt
@@ -42,6 +42,7 @@ class MapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         getCurrentLocation()
         getTime()
